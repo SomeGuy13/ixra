5ad59bea861f5555c07ad75856253161f39435c8
SomeGuy13 <isaac7erickson@gmail.com>
2024-09-15 22:48:19 -0600

    added more to landing page

--- src/app/page.js
+++ src/app/page.js
@@ -11,9 +11,20 @@ export default function Home() {
         guids and training to get you started on your adventures. See you in the stars 
         explorer.
       </p>
+      <h1>Getting Started</h1>
+      <p>Perfect for if your new.</p>
       <Link href="/organization/history"><PageRow title={"History"} description={"A brief history of the past 150 years"}></PageRow></Link>
       <Link href="/organization/systems"><PageRow title={"System Organization"} description={"How we organize our systems"} /></Link>
       <Link href="/organization/planet-classes"><PageRow title={"Planet Classes"} description={"The different classes we use for planets"} /></Link>
+      <h1>Archives</h1>
+      <p>All the data we have collected through years of exploring</p>
+      <Link href="/regions"><PageRow title={"Regions"} description={"List of all known regions"}></PageRow></Link>
+      <PageRow title={"Corporations"} description={"List of all the corporations who explore for the IXRA"}></PageRow>
+      <PageRow title={"Planets"} description={"List of all known planets"}></PageRow>
+      <PageRow title={"Anomolous Objects"} description={"List of all anomolous ojects discovered"}></PageRow>
+      <PageRow title={"Systems"} description={"List of all discovered systems"}></PageRow>
+      <PageRow title={"Logs"} description={"Archives of all submited and discovered logs. Usually people telling of their explorations"}></PageRow>
+      <Link href="/organization/history/full-history"><PageRow title={"History"} description={"Detailed history of how we got to where we are now"}></PageRow></Link>
       <h1>What actually is this?</h1>
       <p>
         This is a little writing project I'm working on, and I figured it would be fun to 
---
+++ src/app/regions/page.js
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <div>
+      <h1>Regions</h1>
+      <p>Regions are sections of space that we use to determine the general area in the galaxy a planet is in.</p>
+    </div>
+  )
+}
