40e13095ae9875e04da25ef1cbc23fb9eb7e423c
SomeGuy13 <isaac7erickson@gmail.com>
2024-09-15 22:31:33 -0600

    added planet classes

--- src/app/organization/planet-classes/page.js
+++ src/app/organization/planet-classes/page.js
@@ -2,6 +2,60 @@ export default function Page() {
   return (
     <div>
       <h1>Planet Classes</h1>
+      <p>
+        We use planet classes to quickly tell what type of planet it is and any other important information about it without having to read too far into it. Typically you will see two classes. The planet itself and its atmosphere. Here is an example of a planet classes:
+      </p>
+      <br/>
+      <p>
+        Cryomara Vacuo
+      </p>
+      <br/>
+      <p> Essentially what those two words are saying is that it is a frozen inhospitable planet with little to no atmosphere. This is handy to know when explorers are trying to plan for a jump so they can quickly see if a planet will have what they need or not. These two classes are separated into the planet class and the atmosphere class. First, lets explain the planet class.
+      </p>
+      <br/>
+      <h2>Planet Class</h2>
+      <p>
+        The planet class is divided into a prefix and a suffix. They are based in Latin and Greek roots and we'll give the quick breakdown here:
+      </p>
+      <br/>
+      <p>Terra- (Earth-like)</p>
+      <p>Aqua- (Water-based)</p>
+      <p>Geo- (Rocky or terrestrial)</p>
+      <p>Cryo- (Frozen)</p>
+      <p>Pryo- (Hot or fire)</p>
+      <p>Atmos- (Gas-based)</p>
+      <p>Xeno- (Unknown)</p>
+      <p>Helio- (Close to sun or very hot)</p>
+      <p>Luna- (Smaller planet)</p>
+      <p>Aeolia- (Strong winds)</p>
+      <br/>
+      <p>After the appropriate prefix is chosen, then the appropriate suffix is chosen which is as follows:</p>
+      <br/>
+      <p>-habis (Habitable)</p>
+      <p>-mara (Hostile or inhospitable)</p>
+      <p>-colonus (Colonized or being settled)</p>
+      <p>-solus (Uninhabited)</p>
+      <p>-domus (Settled)</p>
+      <br/>
+      <p>The suffix and prefix combined can already give some great basic information, but it would also be useful to know the atmosphere of the planet to see if an explorer needs to bring extra equipment to breath, Which brings us to the atmosphere class.</p>
+      <br/>
+      <h2>Atmosphere Class</h2>
+      <p>These are structured the same as planet classes, but they have different prefixes and suffixes. Here are the suffixes:</p>
+      <br/>
+      <p>Ox- (Oxygen rich)</p>
+      <p>Methano- (Methane-based)</p>
+      <p>Hydro- (Hydrogen-based)</p>
+      <p>Nitro- (Nitrogen-heavy)</p>
+      <p>Sulfur- (Sulfuric atmosphere)</p>
+      <p>Vacuo- (Minimal or no atmosphere)</p>
+      <br/>
+      <p>And here are the prefixes:</p>
+      <br/>
+      <p>-tox (Toxic atmosphere)</p>
+      <p>-aer (Breathable atmosphere)</p>
+      <p>-ventus (High winds and storms)</p>
+      <br/>
+      <p>Now many planets and such will have many prefixes and suffixes that can be attatched to them, but for simplicitys sake, if one seems more important than the others, it will be put in as part of the class.</p>
     </div>
   )
 }
